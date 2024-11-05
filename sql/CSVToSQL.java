26b737b21539ccd424865304d996a49bee3d5816
Andrew Jonhardt <70071943+AndrewJDGalga@users.noreply.github.com>
2024-11-05 13:35:48 -0600

    copy-pasted FileReader method, appears I could use in a Utility class, not borthing for now because supposed to be just good enough!

--- sql/CSVToSQL.java
+++ sql/CSVToSQL.java
@@ -1,3 +1,25 @@
+import java.io.FileReader;
+
 class CSVToSQL {
-    
+    public static void main(String[] args) {
+        String fileString = fileContents("test.csv");
+        System.out.println(fileString);
+    }
+
+    public static String fileContents(String path) {
+        StringBuilder result = new StringBuilder();
+
+        try (FileReader f = new FileReader(path);) {
+            int nextChar;
+            while((nextChar = f.read()) != -1){
+                result.append((char) nextChar);
+            }
+            f.close();
+        } catch (Exception e) {
+            result.append("");
+            System.out.println("Filename not found");
+        }
+
+        return result.toString();
+    }
 }
