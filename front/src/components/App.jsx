b1637c8b17e3bf4e870b62d1006c9ab29e3ee421
Guess <contact@andrewjonhardt.com>
2024-12-30 08:34:56 -0600

    initial test objects.

--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -1,6 +1,562 @@
 import { useState } from 'react'
 import '../styling/index.css'
 
+const testObjects = [
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Psycho",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Psycho",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Psycho",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Psycho",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Psycho",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Psycho",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Psycho",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Psycho",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Psycho",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Psycho",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Optical",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Optical",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Optical",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Optical",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Optical",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Optical",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Optical",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Optical",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Optical",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Optical",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Nature",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Nature",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Nature",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Nature",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Nature",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Nature",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Nature",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Nature",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Nature",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Nature",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Ki",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Ki",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Ki",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Ki",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Ki",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Ki",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Ki",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Ki",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Ki",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Ki",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Faith",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Faith",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Faith",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Faith",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Faith",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Faith",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Faith",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Faith",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Faith",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  },
+  {
+    id : 1,
+    name : "Psycho Wave",
+    type : "Attack",
+    school : "Faith",
+    cost : "4",
+    str : "x",
+    use : "1",
+    range : "short",
+    homing : "4"
+  }
+]
+
+const testHeaders = 'ID,NAME,TYPE,SCHOOL,COST,STR,USE,RANGE,HOMING';
+
+
 function App() {
   const [count, setCount] = useState(0)
 
